ee1d0e54e25e7446a506a8cedc9aef74fb401749
Alex Petersen <theoperatore@gmail.com>
2015-01-10 22:12:13 -0500

    accounted for no mass \ cleanup

--- gravitation2d.js
+++ gravitation2d.js
@@ -3,16 +3,15 @@ var v2 = require('./v2');
 module.exports = function solve(obj1, obj2, gravityConstant) {
   gravityConstant = gravityConstant || 0.99;
 
+  // handle either obj not having mass
+  if (!obj1.mass || !obj2.mass) return;
+
   // accel on obj1 because obj2 exists
   var accel1 = v2(); 
-  var out = v2();
   var mag;
   var factor;
 
-  var diffx = obj2.cpos.x - obj1.cpos.x;
-  var diffy = obj2.cpos.y - obj1.cpos.y;
-
-  v2.set(accel1, diffx, diffy);
+  v2.sub(accel1, obj2, obj1);
   mag = v2.magnitude(accel1);
 
   // Newton's Law of Universal Gravitation -- Vector Form!
--- gravitation3d.js
+++ gravitation3d.js
@@ -3,17 +3,15 @@ var v3 = require('./v3');
 module.exports = function solve(obj1, obj2, gravityConstant) {
   gravityConstant = gravityConstant || 0.99;
 
+  // handle either obj not having mass
+  if (!obj1.mass || !obj2.mass) return;
+
   // accel on obj1 because obj2 exists
   var accel1 = v3(); 
-  var out = v3();
   var mag;
   var factor;
 
-  var diffx = obj2.cpos.x - obj1.cpos.x;
-  var diffy = obj2.cpos.y - obj1.cpos.y;
-  var diffz = obj2.cpos.z - obj1.cpos.z;
-
-  v3.set(accel1, diffx, diffy, diffz);
+  v3.sub(accel1, obj2, obj1);
   mag = v3.magnitude(accel1);
 
   // Newton's Law of Universal Gravitation -- Vector Form!
