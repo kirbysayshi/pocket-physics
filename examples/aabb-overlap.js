3de4b9ad261acf2fb1a83cb64ddf7b731d73fdfe
Andrew Petersen <kirbysayshi@gmail.com>
2016-08-02 03:21:21 -0400

    overlap example looks right but is not

--- examples/aabb-overlap.js
+++ examples/aabb-overlap.js
@@ -19,16 +19,16 @@ cvs.style.border = '1px solid gray';
 document.body.appendChild(cvs);
 
 const box1 = {
-  cpos: v2(100, 500),
-  ppos: v2(90, 500),
+  cpos: v2(350, 90),
+  ppos: v2(349, 80),
   acel: v2(),
   w: 100,
   h: 150,
 }
 
 const box2 = {
-  cpos: v2(500, 500),
-  ppos: v2(510, 500),
+  cpos: v2(350, 900),
+  ppos: v2(351, 910),
   acel: v2(),
   w: 100,
   h: 150,
@@ -57,31 +57,34 @@ scihalt(() => running = false);
 
   if (isOverlapping) {
 
+    // for debugging
+    render(points, ctx);
+
     // move to non-overlapping position
     // should the overlap also return the half vectors so both can be moved?
     add(box2.cpos, box2.cpos, collision.resolve);
     add(box2.ppos, box2.ppos, collision.resolve);
 
     // for debugging
-    //render(points, ctx);
+    render(points, ctx);
 
     // reverse the velocity in the collision normal direction
     const reverseVel1 = sub(v2(), box1.ppos, box1.cpos);
-    reverseVel1.x *= collision.normal.x;
-    reverseVel1.y *= collision.normal.y;
+    reverseVel1.x *= collision.normal.x !== 0 ? collision.normal.x : -1;
+    reverseVel1.y *= collision.normal.y !== 0 ? collision.normal.y : -1;
     sub(box1.ppos, box1.cpos, reverseVel1);
 
     // for debugging
-    //render(points, ctx);
+    render(points, ctx);
 
     // reverse the velocity in the collision normal direction
     const reverseVel2 = sub(v2(), box2.ppos, box2.cpos);
-    reverseVel2.x *= collision.normal.x;
-    reverseVel2.y *= collision.normal.y;
+    reverseVel2.x *= collision.normal.x !== 0 ? collision.normal.x : -1;
+    reverseVel2.y *= collision.normal.y !== 0 ? collision.normal.y : -1;
     sub(box2.ppos, box2.cpos, reverseVel2);
 
     // for debugging
-    //render(points, ctx);
+    render(points, ctx);
   }
 
   for (let i = 0; i < points.length; i++) {
