d71ced49e61a3b974d00db6f2e0c67c2f80c4128
Andrew Petersen <kirbysayshi@gmail.com>
2019-07-14 00:58:47 -0400

    remove 3d support

--- src/overlapspheresphere.js
+++
@@ -1,7 +0,0 @@
-export default (ax, ay, az, arad, bx, by, bz, brad) => {
-  const x = bx - ax;
-  const y = by - ay;
-  const z = bz - az;
-  const rad = arad + brad;
-  return x*x + y*y + z*z < rad*rad;
-};
