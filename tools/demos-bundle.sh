f7c59354d608926ade3cf8bfaab7f663e38d3080
Andrew Petersen <kirbysayshi@gmail.com>
2019-07-16 00:30:50 -0400

    remove unused tools, invalidated due to parcel

--- tools/demos-bundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f examples/*-bundle.js;
-
-find examples \
-  -iname '*.ts' \
-  ! -iname '*.d.ts' \
-  -exec sh -c 'browserify --extension .ts {} -o examples/$(basename {} .ts)-bundle.js' \;
