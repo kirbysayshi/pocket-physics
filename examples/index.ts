751c8bad260e4afc4de39812d6180f7644473d83
Andrew Petersen <kirbysayshi@gmail.com>
2019-07-23 23:41:33 -0400

    omg

--- examples/index.ts
+++ examples/index.ts
@@ -4,7 +4,7 @@ import * as CircleCollisions from "./circle-collisions";
 import * as CircleBoxCollision from "./circle-box-collision";
 import * as EdgeCollision from "./edge-collision";
 import * as Platformer from "./platformer";
-import * as Bucket from './bucket';
+import * as Bucket from "./bucket";
 
 const qs = new URLSearchParams(window.location.search);
 const demoName = qs.get("demo");
@@ -16,16 +16,17 @@ const demos = new Map<string, { start: () => void; stop?: () => void }>([
   ["Single Edge Collision (Verlet)", EdgeCollision],
   ["Platformer (AABB Impulse Model)", Platformer],
   ["AABB Overlap Demo (AABB Impulse Model)", AABBOverlapDemo],
-  ["AABB Soup Demo (AABB Impulse Model)", AABBSoupDemo],
+  ["AABB Soup Demo (AABB Impulse Model)", AABBSoupDemo]
 ]);
 
 if (demoName && demos.has(demoName)) {
   demos.get(demoName)!.start();
 } else {
   const names = Array.from(demos.keys());
+  const url = `${window.location.pathname}?demo=${encodeURIComponent(name)}`;
 
   const li = (name: string) => `
-    <li><a href="?demo=${encodeURIComponent(name)}">${name}</a></li>
+    <li><a href="${url}">${name}</a></li>
   `;
 
   const html = `
