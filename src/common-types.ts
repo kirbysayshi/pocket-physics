b95c5fec0c581d39f21419ecaff11fb51027ec66
Andrew Petersen <kirbysayshi@gmail.com>
2022-08-25 15:58:12 -0400

    fix: add generic defaults to common types

--- src/common-types.ts
+++ src/common-types.ts
@@ -1,11 +1,11 @@
 import { Vector2 } from "./v2";
 
-export type Integratable<T extends number> = {
+export type Integratable<T extends number = number> = {
   cpos: Vector2<T>;
   ppos: Vector2<T>;
   acel: Vector2<T>;
 };
-export type VelocityDerivable<T extends number> = Pick<
+export type VelocityDerivable<T extends number = number> = Pick<
   Integratable<T>,
   "cpos" | "ppos"
 >;
